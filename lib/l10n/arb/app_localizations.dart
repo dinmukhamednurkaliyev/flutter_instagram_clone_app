d7c2c2e6230f65750accf6a5d7a471d893128c93
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-26 12:11:13 +0500

    chore(l10n): Remove Spanish language support

--- lib/l10n/arb/app_localizations.dart
+++ lib/l10n/arb/app_localizations.dart
@@ -6,7 +6,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart' as intl;
 
 import 'app_localizations_en.dart';
-import 'app_localizations_es.dart';
 
 // ignore_for_file: type=lint
 
@@ -91,8 +90,7 @@ abstract class AppLocalizations {
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
-    Locale('en'),
-    Locale('es')
+    Locale('en')
   ];
 
   /// Text shown in the AppBar of the Counter Page
@@ -111,7 +109,7 @@ class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> 
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['en', 'es'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => <String>['en'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_AppLocalizationsDelegate old) => false;
@@ -123,7 +121,6 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
     case 'en': return AppLocalizationsEn();
-    case 'es': return AppLocalizationsEs();
   }
 
   throw FlutterError(
