e6241055f844f47ef8e604c147cfa9179df1fdf9
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-26 09:52:20 +0500

    feat: Initial project setup

---
+++ android/app/src/main/kotlin/com/dinmukhamed/nurkaliyev/flutter/instagram/clone/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dinmukhamed_nurkaliyev.flutter_instagram_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_instagram_clone_app/app/app.dart';
+import 'package:flutter_instagram_clone_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
